0a6c2af7b7faa3dc1d4ee9497232a68194166744
Aritra-art <aritrachowdhury65@gmail.com>
2023-05-22 22:02:06 +0530

    feat: brand filter from home page

--- src/components/TopBrands.jsx
+++ src/components/TopBrands.jsx
@@ -1,16 +1,25 @@
+import { useContext } from "react";
+import { FilterContext } from "../context/FilterContext";
 import { brandImgArr } from "../utils/brandImgArr";
 import "./TopBrands.css";
+import { useNavigate } from "react-router";
 
 export const TopBrands = () => {
+  const { dispatchFilter } = useContext(FilterContext);
+  const navigate = useNavigate();
   return (
     <>
       <h1 className="newarrival-header">Top Brands</h1>
       <div className="top-brands-section">
-        {brandImgArr.map((url) => {
+        {brandImgArr.map(({ id, brand, url }) => {
           return (
             <img
+              onClick={() => {
+                dispatchFilter({ type: "SET_BRAND_FILTER", payload: brand });
+                navigate("/products");
+              }}
               src={url}
-              key={url}
+              key={id}
               alt="url"
               width="200px"
               className="brand-image-tile"
