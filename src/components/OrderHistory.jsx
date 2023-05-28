70f5997e2eeb46fc427c3db0e459debbe373520f
Aritra-art <aritrachowdhury65@gmail.com>
2023-05-28 18:00:45 +0530

    style:order history

--- src/components/OrderHistory.jsx
+++ src/components/OrderHistory.jsx
@@ -37,55 +37,59 @@ export const OrderHistory = () => {
                 paymentId,
               }) => {
                 return (
-                  <li className="order-history" key={id}>
-                    <p>
-                      <span className="order-placeholder">Order ID</span>
-                      {id}
-                    </p>
-                    <p>
-                      <span className="order-placeholder">Order Date</span>
-                      {orderDate}
-                    </p>
-                    <p>
-                      <span className="order-placeholder">Order Time</span>
-                      {orderTime}
-                    </p>
-                    <h2 className="order-heading">Ordered Items</h2>
+                  <div className="order-history" key={id}>
+                    <div>
+                      <p>
+                        <span className="order-placeholder">Order ID</span>
+                        {id}
+                      </p>
+                      <p>
+                        <span className="order-placeholder">Payment ID</span>
+                        {paymentId}
+                      </p>
+                      <p>
+                        <span className="order-placeholder">Order Date</span>
+                        {orderDate}
+                      </p>
+                      <p>
+                        <span className="order-placeholder">Order Time</span>
+                        {orderTime}
+                      </p>
+                      <p>
+                        <span className="order-placeholder">Order Amount</span>
+                        {amount}
+                      </p>
+                      <div>
+                        <h2 style={{ fontSize: "1.3rem" }}>
+                          {address.userName}
+                        </h2>
+                        <p>
+                          {address?.houseNumber}, {address.city},{" "}
+                          {address?.city}
+                        </p>
+                        <p>Pincode: {address?.pincode}</p>
+                        <p>Ph No: {address?.mobileNumber}</p>
+                      </div>
+                    </div>
                     <div className="order-history-order-layout">
                       {order &&
                         order?.map(({ id, title, qty, thumbnail }) => {
                           return (
-                            <div className="orderhistory-order" key={id}>
-                              <img src={thumbnail} width="130px" alt={title} />
-                              <span className="product-order-content">
+                            <div className="product-order-history" key={id}>
+                              <img
+                                src={thumbnail}
+                                width="100px"
+                                height="100px"
+                                alt={title}
+                              />
+                              <span className="product-order-content-history">
                                 {title} X {qty}
                               </span>
                             </div>
                           );
                         })}
                     </div>
-
-                    <h2 className="order-heading">Delivered Address</h2>
-                    <div>
-                      <h2 style={{ fontSize: "1.3rem" }}>{address.userName}</h2>
-                      <p>
-                        <span style={addressPlaceholder}>House Number:</span>{" "}
-                        {address?.houseNumber}{" "}
-                        <span style={addressPlaceholder}>Area:</span>{" "}
-                        {address.area}
-                      </p>
-                      <p>
-                        <span style={addressPlaceholder}>City:</span>{" "}
-                        {address.city}
-                      </p>
-                      <p>
-                        <span style={addressPlaceholder}>Pincode:</span>{" "}
-                        {address?.pincode}{" "}
-                        <span style={addressPlaceholder}>Ph No.</span>{" "}
-                        {address.mobileNumber}
-                      </p>
-                    </div>
-                  </li>
+                  </div>
                 );
               }
             )}
